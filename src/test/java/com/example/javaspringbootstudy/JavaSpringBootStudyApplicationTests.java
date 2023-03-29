43f6a8dd4e8bf1c2c98e2e48e302d5d6803ccef0
diiiiiiiino <eodyddnjs@naver.com>
2023-03-29 13:45:31 +0900

    init

---
+++ src/test/java/com/example/javaspringbootstudy/JavaSpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
