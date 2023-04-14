0814426e0a18454ccc6f3dcba411bbd53e58f91d
diiiiiiiino <eodyddnjs@naver.com>
2023-04-14 14:16:33 +0900

    Map 기능 테스트

--- src/test/java/모던자바인액션/CollectionTests.java
+++ src/test/java/모던자바인액션/CollectionTests.java
@@ -7,6 +7,7 @@ import 모던자바인액션.model.Trader;
 import 모던자바인액션.model.Transaction;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CollectionTests {
 
@@ -111,7 +112,7 @@ public class CollectionTests {
     @DisplayName("8.3.2 정렬 메서드 comparingByKey comparingByValue")
     @Test
     void test8_3_2() {
-        Map<String, String> favouriteMovies = getFavouriteMovies();
+        Map<String, String> favouriteMovies = getImmutableFavouriteMovies();
 
         favouriteMovies.entrySet()
                 .stream()
@@ -122,16 +123,194 @@ public class CollectionTests {
     @DisplayName("8.3.3 getOrDefault 메서드")
     @Test
     void test8_3_3() {
-        Map<String, String> favouriteMovies = getFavouriteMovies();
+        Map<String, String> favouriteMovies = getImmutableFavouriteMovies();
 
         Assertions.assertEquals("Star Wars", favouriteMovies.getOrDefault("Raphael", "NONE"));
         Assertions.assertEquals("NONE", favouriteMovies.getOrDefault("NOS", "NONE"));
     }
 
-    public static Map<String, String> getFavouriteMovies(){
+    @DisplayName("8.3.4 계산 패턴")
+    @Test
+    void test8_3_4() {
+        Map<String, ArrayList<String>> movies = getFavouriteMovies2();
+
+        movies.computeIfAbsent("Raphael", key -> new ArrayList<>()); //값이 null이거나
+        Assertions.assertEquals(3, movies.size());
+
+        movies.computeIfAbsent("Raphael2", key -> new ArrayList<>()); //키가 없으면 실행한다.
+        Assertions.assertEquals(4, movies.size());
+
+        Map<String, ArrayList<String>> movies2 = getFavouriteMovies2();
+
+        movies2.computeIfPresent("Raphael", (s, strings) -> new ArrayList<>()); // 값이 null이면 실행 안함
+        Assertions.assertNull(movies2.get("Raphael"));
+
+        movies2.computeIfPresent("Raphael2", (s, strings) -> new ArrayList<>());
+        Assertions.assertNull(movies2.get("Raphael2")); // 키가 없으면 실행 안함
+
+        movies2.computeIfPresent("Olivia", (s, strings) -> null); // 값을 만드는 함수가 널을 반환하면 현재 매핑을 맵에서 제거한다. 권장되지 않음 remove 사용 권장
+        Assertions.assertNull(movies2.get("Olivia"));
+
+        movies2.computeIfPresent("Cristina", (s, strings) -> {
+            strings.add("hihi");
+            return strings;
+        });
+
+        Assertions.assertEquals(1, movies2.get("Cristina").size());
+
+        Map<String, ArrayList<String>> movies3 = getFavouriteMovies2();
+        movies3.compute("Raphael", (s, strings) -> new ArrayList<>());
+        Assertions.assertNotNull(movies3.get("Raphael"));
+
+        movies3.compute("Raphael2", (s, strings) -> new ArrayList<>());
+        Assertions.assertNotNull(movies3.get("Raphael2"));
+    }
+
+    @DisplayName("8.3.5 삭제 패턴")
+    @Test
+    void test8_3_5() {
+        Map<String, String> movies = getFavouriteMovies();
+        movies.remove("Olivia");
+
+        Assertions.assertEquals(2, movies.size());
+
+        Map<String, String> movies2 = getFavouriteMovies();
+
+        movies2.remove("Olivia", "James Bond");
+
+        Assertions.assertEquals(2, movies2.size());
+        Assertions.assertNull(movies2.get("Olivia"));
+
+        Map<String, String> movies3 = getFavouriteMovies();
+
+        movies3.remove("Olivia", "NONE");
+
+        Assertions.assertEquals(3, movies3.size());
+        Assertions.assertNotNull(movies3.get("Olivia"));
+    }
+
+    @DisplayName("8.3.6 교체 패턴")
+    @Test
+    void test8_3_6() {
+        Map<String, String> movies = getFavouriteMovies();
+        movies.replace("Raphael", "Star wars2");
+        movies.replaceAll((s, s2) -> s2.toUpperCase());
+
+        Assertions.assertEquals("STAR WARS2", movies.get("Raphael"));
+    }
+
+    @DisplayName("8.3.7 합침")
+    @Test
+    void test8_3_7() {
+        Map<String, String> family = Map.ofEntries(
+                Map.entry("Teo", "Star Wars"),
+                Map.entry("Cristina", "James Bond"));
+
+        Map<String, String> friends = Map.ofEntries(
+                Map.entry("Raphael", "Star Wars"),
+                Map.entry("Cristina", "Matrix"));
+
+        Map<String, String> everyone = new HashMap<>(family);
+        friends.forEach((k, v) -> everyone.merge(k, v, (movie1, movie2) -> movie1 + " & " + movie2));
+
+        Assertions.assertEquals(3, everyone.size());
+        Assertions.assertEquals("James Bond & Matrix", everyone.get("Cristina"));
+
+        /**지정된 키와 연관된 값이 없을때 키를 널이 아닌 값과 연결한다.**/
+        Map<String, Long> moviesToCount = new HashMap<>();
+        moviesToCount.merge("Matrix", 1L, (oldValue, newValue) -> newValue + 1L);
+        Assertions.assertEquals(1, moviesToCount.get("Matrix"));
+
+        moviesToCount.merge("Matrix", 1L, (oldValue, newValue) -> newValue + 1L);
+        Assertions.assertEquals(2, moviesToCount.get("Matrix"));
+
+        /**지정된 키와 연관된 값이 널이면 키를 널이 아닌 값과 연결한다.**/
+        Map<String, Long> moviesToCount2 = new HashMap<>();
+        moviesToCount2.put("Matrix", null);
+
+        moviesToCount2.merge("Matrix", 1L, (oldValue, newValue) -> newValue + 1L);
+        Assertions.assertEquals(1, moviesToCount2.get("Matrix"));
+
+        moviesToCount2.merge("Matrix", 3L, (oldValue, newValue) -> newValue + 1L);
+        Assertions.assertEquals(4, moviesToCount2.get("Matrix"));
+
+        /** Matrix의 값이 없거나 널이면 아래의 BiFunction은 수행되지 않는다 or 값이 있으면 BiFunction이 수행된다 **/
+
+        /**연결된 값을 주어진 매핑 함수의 결과가 널이면 항목을 제거한다**/
+        moviesToCount2.merge("Matrix", 1L, (oldValue, newValue) -> null);
+        Assertions.assertNull(moviesToCount2.get("Matrix"));
+
+        Map<String, Long> moviesToCount3 = new HashMap<>();
+        moviesToCount3.put("Matrix", 1L);
+
+        moviesToCount3.merge("Matrix", 1L, (oldValue, newValue) -> newValue + 1L);
+        Assertions.assertEquals(2, moviesToCount3.get("Matrix"));
+
+        moviesToCount3.merge("Matrix", 3L, (oldValue, newValue) -> newValue + 1L);
+        Assertions.assertEquals(4, moviesToCount3.get("Matrix"));
+    }
+
+    @DisplayName("8.4.1 ConcurrentHashMap 리듀스와 검색")
+    @Test
+    void test8_4_1(){
+        ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+        map.put("a", 1L);
+        map.put("b", 10L);
+        map.put("c", 100L);
+
+        long parallelismThreshold = 1;
+        Optional<Long> maxValue = Optional.ofNullable(map.reduceValues(parallelismThreshold, Long::max));
+
+        Assertions.assertEquals(100, maxValue.get());
+    }
+
+    @DisplayName("8.4.2 ConcurrentHashMap 계수")
+    @Test
+    void test8_4_2() {
+        ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+        map.put("a", 1L);
+        map.put("b", 10L);
+        map.put("c", 100L);
+
+        Assertions.assertEquals(3, map.mappingCount());
+    }
+
+    @DisplayName("8.4.3 ConcurrentHashMap 집합뷰")
+    @Test
+    void test8_4_3() {
+        ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+        map.put("a", 1L);
+        map.put("b", 10L);
+        map.put("c", 100L);
+
+        ConcurrentHashMap.KeySetView<String, Long> keySetView = map.keySet();
+        map.put("d", 200L);
+
+        Assertions.assertEquals(4, keySetView.size());
+    }
+
+    public static Map<String, String> getImmutableFavouriteMovies(){
         return Map.ofEntries(
                 Map.entry("Raphael", "Star Wars"),
                 Map.entry("Cristina", "Matrix"),
                 Map.entry("Olivia", "James Bond"));
+    }
+
+    public static Map<String, String> getFavouriteMovies(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Raphael", "Star Wars");
+        map.put("Cristina", "Matrix");
+        map.put("Olivia", "James Bond");
+
+        return map;
+    }
+
+    public static Map<String, ArrayList<String>> getFavouriteMovies2(){
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        map.put("Raphael", null);
+        map.put("Cristina", new ArrayList<>());
+        map.put("Olivia", new ArrayList<>());
+
+        return map;
     }
 }
